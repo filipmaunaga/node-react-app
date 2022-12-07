d32a3a50f1995acecfc7ed13ed6ddbfa2a9ee988
Filip Maunaga <filip.maunaga@jsguru.io>
2022-12-07 12:33:37 +0100

    convert to typescript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<null>(null);
 
   useEffect(() => {
     fetch("/api")
-      .then((res) => res.json())
+      .then((res: Response) => res.json())
       .then((data) => setData(data.message));
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
---
+++ server/index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
+const app: Express = express();
+
+app.get("/api", (req: Request, res: Response) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
