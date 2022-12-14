8eccbacc2ff9879639372b79ba0425d5781d8a1d
Filip Maunaga <filip.maunaga@jsguru.io>
2022-12-14 13:39:18 +0100

    add mui to frontend, setup routes, header draft

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,41 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import axios from "axios";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./App.css";
+import Header from "./components/Header";
+import Compose from "./pages/Compose";
 
 function App() {
-  const [data, setData] = useState<any[]>([]);
+  const theme = createTheme({});
+  //  const [data, setData] = useState<any[]>([]);
 
-  const getData = async () => {
-    const res = await axios.get("/posts");
-    console.log(res.data);
-    setData(res.data);
-  };
+  // const getData = async () => {
+  //   const res = await axios.get("/posts");
+  //   console.log(res.data);
+  //   setData(res.data);
+  // };
 
   // useEffect(() => {
-  //   fetch("/api")
-  //     .then((res: Response) => res.json())
-  //     .then((data) => setData(data.message));
+  //   getData();
   // }, []);
 
-  useEffect(() => {
-    getData();
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Routes>
+          <Route path="/compose" element={<Compose />} />
+        </Routes>
         <div>
-          {!data ? (
+          {/* {!data ? (
             <p>"Loading..."</p>
           ) : (
             data.map((post) => <p key={post._id}>{post.postTitle}</p>)
-          )}
+          )} */}
         </div>
-      </header>
-    </div>
+      </ThemeProvider>
+    </Router>
   );
 }
 
